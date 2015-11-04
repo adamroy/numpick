b9802a2daec14b6fcde0891f410725ebecbc7725
Adam <adam@playnimbus.com>
2015-11-04 17:26:25 -0500

    Neural network test code.

---
+++ src/numpick/NeurophTest.java
@@ -0,0 +1,75 @@
+package numpick;
+
+import java.util.Arrays;
+
+import org.neuroph.core.NeuralNetwork;
+import org.neuroph.core.data.DataSet;
+import org.neuroph.core.data.DataSetRow;
+import org.neuroph.core.learning.LearningRule;
+import org.neuroph.nnet.MultiLayerPerceptron;
+import org.neuroph.nnet.Perceptron;
+import org.neuroph.nnet.learning.BackPropagation;
+import org.neuroph.nnet.learning.KohonenLearning;
+
+public class NeurophTest
+{
+	public static void main(String[] args)
+	{
+		
+		/*
+		NeuralNetwork<BackPropagation> neuralNetwork = new MultiLayerPerceptron(1);
+		DataSet trainingSet = new DataSet(2, 1);
+
+		// add training data to training set (logical OR function)
+		trainingSet.addRow(new DataSetRow(new double[] { 0, 0 }, new double[] { 0 }));
+		trainingSet.addRow(new DataSetRow(new double[] { 0, 1 }, new double[] { 1 }));
+		trainingSet.addRow(new DataSetRow(new double[] { 1, 0 }, new double[] { 1 }));
+		trainingSet.addRow(new DataSetRow(new double[] { 1, 1 }, new double[] { 1 }));
+		
+		// learn the training set
+		neuralNetwork.learn(trainingSet);
+		
+		// save the trained network into file
+		neuralNetwork.save("or_perceptron.nnet");
+		*/
+        // create training set (logical AND function)
+        DataSet trainingSet = new DataSet(2, 1);
+        trainingSet.addRow(new DataSetRow(new double[]{0, 0}, new double[]{0}));
+        trainingSet.addRow(new DataSetRow(new double[]{0, 1}, new double[]{0}));
+        trainingSet.addRow(new DataSetRow(new double[]{1, 0}, new double[]{0}));
+        trainingSet.addRow(new DataSetRow(new double[]{1, 1}, new double[]{1}));
+
+        // create perceptron neural network
+        NeuralNetwork myPerceptron = new Perceptron(2, 1);
+        // learn the training set
+        myPerceptron.learn(trainingSet);
+        // test perceptron
+        System.out.println("Testing trained perceptron");
+        testNeuralNetwork(myPerceptron, trainingSet);
+        // save trained perceptron
+        myPerceptron.save("mySamplePerceptron.nnet");
+        // load saved neural network
+        NeuralNetwork loadedPerceptron = NeuralNetwork.load("mySamplePerceptron.nnet");
+        // test loaded neural network
+        System.out.println("Testing loaded perceptron");
+        testNeuralNetwork(loadedPerceptron, trainingSet);
+        
+        
+        loadedPerceptron.setInput(1.0, 0.0);
+        loadedPerceptron.calculate();
+        double[] out = loadedPerceptron.getOutput();
+        System.out.println("My output: " + Arrays.toString(out));
+	}
+	
+    public static void testNeuralNetwork(NeuralNetwork neuralNet, DataSet testSet) {
+
+        for(DataSetRow trainingElement : testSet.getRows()) {
+            neuralNet.setInput(trainingElement.getInput());
+            neuralNet.calculate();
+            double[] networkOutput = neuralNet.getOutput();
+
+            System.out.print("Input: " + Arrays.toString(trainingElement.getInput()) );
+            System.out.println(" Output: " + Arrays.toString(networkOutput) );
+        }
+    }
+}
